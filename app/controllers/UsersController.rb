61a6b7b6dd1ee4d325e01863e03e8bd860e591fc
Lucas Gutierrez <70801680+LuGuDu@users.noreply.github.com>
2022-03-29 20:20:36 +0200

    improving the user views

--- app/controllers/UsersController.rb
+++ app/controllers/UsersController.rb
@@ -1,8 +1,66 @@
 class UsersController < ApplicationController
     protect_from_forgery with: :null_session
+    
     def new
         @user = User.new
+    end
+
+    def list
+      @users = User.all
+      respond_to do |format|
+          format.html { render template: 'users/list', layout: 'layouts/application', status: 200}
       end
+    end
+
+    def delete
+      @user = User.find(params[:id])
+      if @user.destroy
+          redirect_to "/user"
+      else
+      end
+  end
+
+  def update_form
+    @user = User.find(params[:id])
+    respond_to do |format|
+        format.html { render template: 'user/update', layout: 'layouts/application', status: 200}
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.username = params[:username]
+    @user.email = params[:email]
+    @user.password = params[:password]
+
+    if @user.save
+        redirect_to "/user"
+    else
+        #redirect_to "/project"
+    end
+end
+
+def create_form
+  respond_to do |format|
+    format.html { render template: 'users/create', layout: 'layouts/application', status: 200}
+end
+end
+
+def create_from_admin
+  @user = User.new
+  @user.username=params[:username]
+  @user.email=params[:email]
+  @user.password=params[:password]
+  @user.password_confirmation=params[:password_confirmation]
+
+  if @user.save
+    session[:user_id]=@user.id
+    flash[:notice] = "User created."
+    redirect_to '/user'
+  else
+    render 'new'
+  end
+end
       
     def create
         @user = User.new
